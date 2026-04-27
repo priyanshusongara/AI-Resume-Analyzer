6e005d13d66dcd5b5308762084f08de563f581ac
priyanshusongara <priyanshusongara23@gmail.com>
2026-04-27 20:36:14 +0530

    bycryptfix

--- app/auth.py
+++ app/auth.py
@@ -10,7 +10,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 60
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+pwd_context = CryptContext(schemes=["bcrypt_sha256"], deprecated="auto")
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
